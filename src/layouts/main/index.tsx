119408975d4bd49c2201c9496d8b074c69773287
Yazdan Vahidi Sharbaaf <yazdanvahidi@yahoo.com>
2024-08-08 16:49:07 +0330

    feat: add routes

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import { MainRouter } from "../../routers";
 import routes from "../../routers/main/routes";
 import styles from "./MainLayout.module.scss";
@@ -6,22 +7,23 @@ import styles from "./MainLayout.module.scss";
 const MainLayout: FC = () => {
   return (
     <main className={styles.container}>
-      <section className={styles.content}>
-        <MainRouter routes={routes} />
-      </section>
+      <Router>
+        <section className={styles.content}>
+          <MainRouter routes={routes} />
+        </section>
 
-      <aside className={styles.sidebar}>
-        {/* TODO: Convert the list into a component */}
-        <ul className={styles.sidebarList}>
-          {routes.map((route) => (
-            <li key={route.to} className={styles.sidebarItem}>
-              <a href={route.to} className={styles.sidebarLink}>
-                {route.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </aside>
+        <aside className={styles.sidebar}>
+          <ul className={styles.sidebarList}>
+            {routes.map((route) => (
+              <li key={route.to} className={styles.sidebarItem}>
+                <Link to={route.to} className={styles.sidebarLink}>
+                  {route.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </aside>
+      </Router>
     </main>
   );
 };
---
+++ src/pages/SelectedCategories.page.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react";
+
+const SelectedCategoriesPage: FC = () => {
+  return <div>SelectedCategoriesPage</div>;
+};
+export default SelectedCategoriesPage;
